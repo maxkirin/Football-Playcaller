4803929483e0abc63203c786a482c0feda5f6013
RojoDiego <60203730+RojoDiego@users.noreply.github.com>
2020-02-26 16:52:03 -0500

    Current state of deployed Firebase

---
+++ app.js
@@ -0,0 +1,7 @@
+function submitData(e){
+e.preventDefault();
+document.getElementById('container2').style.visibility = 'visible';
+}
+
+const form = document.getElementById('situations');
+form.addEventListener('submit', submitData);
