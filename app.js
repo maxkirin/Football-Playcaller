cf13067bec13b9ce1d242647ece9a61c7d719107
kcressman <54919012+kcressman@users.noreply.github.com>
2020-03-29 17:24:21 -0400

    updated with table

--- app.js
+++ app.js
@@ -12,8 +12,12 @@ var buttond = document.getElementById("data");
 var head1 = document.getElementById("head1");
 
 function ShowData(){
+	var input = text.value;
 	var db = firebase.database();					//up to football-playcaller-f3ade
 	var ref = db.ref().child("week2");				//up to parent node that we need access to
+	/*if (input.equals(ref.child("season"))){
+			$("#sea").append("<tr>"+season+"</tr>");
+		}*/
 	ref.on("child_added", snap=>{					//child_added event to retrieve list of items
 													//snap like (function(snapshot))
 		var away_score = snap.child("away_score").val();		//each value of child key stored
@@ -26,12 +30,13 @@ function ShowData(){
 		var state_of_game = snap.child("state_of_game").val();
 		var type = snap.child("type").val();
 		var week = snap.child("week").val();
-		//creates a table to add in "enterScores.html"
-		$("#table_body").append("<tr><td>"+away_score+"</td><td>"+away_team+"</td><td>"+game_id+
+		
+		
+		//creates a table to add in "enterScores.html" using jquery
+		$("#result").append("<tr><td>"+away_score+"</td><td>"+away_team+"</td><td>"+game_id+
 						"</td><td>"+game_url+"</td><td>"+home_score+"</td><td>"+home_team+
 						"</td><td>"+season+"</td><td>"+type+"</td><td>"+week+"</td></tr>");
 	})
-	
 }	
 
 function WriteData(){								//when send clicked creates new child
