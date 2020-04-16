bba4d73633969801c19aadc71cdf1dac83e1474c
kcressman <54919012+kcressman@users.noreply.github.com>
2020-04-16 17:03:52 -0400

    fixed to read new DB and has calculations

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+//Key which allows access to the various functions of Firebase without needing user account and authentication
+var config = {
+	"apiKey": "AIzaSyAGyoxhLyCEyJo1Km37xvwKyAyQiR00Z_g",
+	"databaseURL": "https://football-playcaller-f3ade.firebaseio.com",
+	"storageBucket": "football-playcaller-f3ade.appspot.com",
+	"authDomain": "football-playcaller-f3ade.firebaseapp.com",
+	"projectId": "1:180449355890:web:a3b76fdf536458dab97aeb"
+};
+firebase.initializeApp(config);
+
 function submitData(e){
 e.preventDefault();
 document.getElementById('container2').style.visibility = 'visible';		//container 2 is taken from enterscores.html
@@ -6,43 +16,55 @@ document.getElementById('container2').style.visibility = 'visible';		//container
 const form = document.getElementById('situations');						//div:container 1; form: situations
 form.addEventListener('submit', submitData);				//when confirm clicked show container 2
 
-var text = document.getElementById("text");
+//var text = document.getElementById("txt");
 var buttonw = document.getElementById("write");
 var buttond = document.getElementById("data");
 var head1 = document.getElementById("head1");
 
 function ShowData(){
-	var input = text.value;
+	//var input = text.value;
 	var db = firebase.database();					//up to football-playcaller-f3ade
-	var ref = db.ref().child("week2");				//up to parent node that we need access to
+	var ref = db.ref();							//up to parent node that we need access to
+	var count;
+	//Displays how many score differentials are zero
+	var testref = ref.orderByChild("AbsScoreDiff").equalTo(59);
+	testref.on("value", snap=>{		
+		count = 0;
+		snap.forEach(function(childsnap) {
+			count = count + (childsnap.child("AbsScoreDiff").val());
+			console.log(childsnap.child("AbsScoreDiff").val());
+		});
 	
-	//ref.orderByValue() - will print the whole table and the number nodes
-	//the way it is now will display the away score in order; only showing the highest value
-	ref.orderByChild("away_score").limitToLast(1).on("child_added", snap=>{		
+		console.log(count);
+		//Run
+		$("#totPR").append("\t"+count);
+		$("#avgYR").append("\t"+count);
+		$("#numR").append("\t"+count);
+		$("#convR").append("\t"+count+"%");
+		$("#lossyards").append("\t"+count+"%");
+		//Pass
+		$("#totPP").append("\t"+count);
+		$("#comp").append("\t"+count+"%");
+		$("#inter").append("\t"+count);
+		$("#sack").append("\t"+count);
+		$("#avgYP").append("\t"+count);
+		$("#numP").append("\t"+count);
+		$("#convP").append("\t"+count+"%");
+		//Table
+		var avgRun = count / 20;
+		$("#avgrun").append("\t"+avgRun);
+		var avgPass = count * 20;
+		$("#avgpass").append("\t"+avgPass);
+		
+		
+		//displays all values(15704) of "AbsScoreDiff"
+	//ref.orderByChild("AbsScoreDiff").startAt(0).limitToLast(5).on("child_added", function(data){		
 																			//child_added event to retrieve list of items
-													
-		var away_score = snap.child("away_score").val();		//each value of child key stored
-		var away_team = snap.child("away_team").val();
-		var game_id = snap.child("game_id").val();
-		var game_url = snap.child("game_url").val();
-		var home_score = snap.child("home_score").val();
-		var home_team = snap.child("home_team").val();
-		var season = snap.child("season").val();
-		var state_of_game = snap.child("state_of_game").val();
-		var type = snap.child("type").val();
-		var week = snap.child("week").val();
+		//var num = data.val();
+		//console.log("abs score: "+num.AbsScoreDiff);
+		//var away_score = (num.AbsScoreDiff);
+		//$("#totPR").append("\t"+count);
 		
-		
-		$("#awayS").append("\t"+away_score);
-		$("#awayT").append("\t"+away_team);
-		$("#gameI").append("\t"+game_id);
-		$("#gameU").append("\t"+game_url);
-		$("#homeS").append("\t"+home_score);
-		$("#homeT").append("\t"+home_team);
-		$("#seaS").append("\t"+season);
-		$("#stateG").append("\t"+state_of_game);
-		$("#typE").append("\t"+type);
-		$("#weeK").append("\t"+week);
 				
 	})
 }	
